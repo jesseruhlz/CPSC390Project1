35c6d7308f71d310311e8ee66d4365f556cce2e7
Jesse Ruhl <60628685+jesseruhlz@users.noreply.github.com>
2022-09-11 21:36:51 -0700

    created node class

--- depthfirstsearch.py
+++ depthfirstsearch.py
@@ -3,3 +3,15 @@
 # Everett Prussak
 # CPSC 390
 # Project 1
+
+# Node
+class Node():
+    def _init_(self, state, edgeWeight = 0):
+        self.state = state # Letter
+        self.edgeWeight = edgeWeight # Cost from parent to child Node
+        self.children = []
+
+    def addChild(self, childNode, edgeWeight):
+        childNode.parent = self
+        childNode.edgeWeight = edgeWeight
+        self.children.append(childNode)
