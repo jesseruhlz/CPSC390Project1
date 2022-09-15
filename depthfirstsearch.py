7ffc2ec7da3297d132f63e34a9adc14f6c8011d0
Jesse Ruhl <60628685+jesseruhlz@users.noreply.github.com>
2022-09-14 18:44:00 -0700

    fixed errors in node and tree class, and created tree with nodes

--- depthfirstsearch.py
+++ depthfirstsearch.py
@@ -11,9 +11,9 @@
 
 # Node
 class Node():
-    def _init_(self, state, edgeWeight = 0):
+    def __init__(self, state, edgeWeight=0):
         self.state = state # Letter
-        self.edgeWeight = edgeWeight # Cost from parent to child Node
+        self.edgeWeight = edgeWeight # Cost from parent to self
         self.children = []
 
     def addChild(self, childNode, edgeWeight):
@@ -23,7 +23,7 @@ class Node():
 
 # Tree
 class Tree():
-    def _init_(self):
+    def __init__(self):
         self.root = None
         self.path = []
         self.visited = []
@@ -31,7 +31,7 @@ class Tree():
 
     # add the root to the Tree
     def addRoot(self, rootNode):
-        self.rootNode = rootNode
+        self.root = rootNode
 
     # add the node to the Tree
     def addNode(self, childNode, parentNode, edgeWeight):
@@ -51,7 +51,34 @@ class Tree():
 
         # add children nodes to frontier
         for n in currentNode.children:
-            self.depthFirstSearch(n, destintaion)
+            self.depthFirstSearch(n, destination)
             # if currentNode is not in path then remove it
             if (not self.destinationFound):
                 self.path.pop()
+
+# create an object of the Tree
+tree = Tree()
+
+# create nodes for Tree
+root = Node("S")
+a = Node("A")
+b = Node("B")
+c = Node("C")
+d = Node("D")
+e = Node("E")
+g = Node("G")
+
+tree.addRoot(root)
+tree.addNode(a, tree.root, 3)
+tree.addNode(b, tree.root, 1)
+tree.addNode(c, tree.root, 8)
+tree.addNode(d, a, 3)
+tree.addNode(e, a, 7)
+tree.addNode(g, a, 15)
+tree.addNode(g, b, 20)
+tree.addNode(g, c, 5)
+
+tree.depthFirstSearch(tree.root, g)
+
+print("Expanded node: " + str(tree.visited))
+print("The resulting path: " + str(tree.path))
